d91e77dd6ef250a2cca3a1fc7612d0473c679184
MaianneThornton <maianne.thornton@gmail.com>
2021-09-20 16:53:47 -0400

    Completed Bonus Questions. Exercise is complete.

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ console.log(9);
 // +, -, *, /, **, %
 
 // 4
-console.log(5%2);
-console.log(5%4);
+console.log(5%2); // 5 divided by 2 equals 2 R1
+console.log(5%4); // 5 divided by 4 equals 1 R1
 
 // 5
 console.log('I\'M MARY POPPINS, Y\'ALL!');
@@ -22,3 +22,28 @@ console.log("Maianne".length + "Thornton".length);
 
 // 8
 console.log("Avengers: Endgame"[16]);
+
+// BONUS BONUS
+// 1
+console.log("/");
+
+// 2
+console.log(5**4-9);
+
+// 3
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]); 
+console.log("OKOYE"[4]); 
+console.log("NICK FURY"[0]); 
+console.log("GROOT"[0]); 
+console.log("DOCTOR STRANGE"[13]); 
+console.log("THOR"[3]); 
+console.log("STAR-LORD"[0]);
+
+// 5
+var m=25;
+var c=4;
+var u= m + c;
