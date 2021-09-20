7d246c4bb10a72cf6fd60414b5ecc1987c77d275
MaianneThornton <maianne.thornton@gmail.com>
2021-09-20 15:19:46 -0400

    Completed Exercise.

---
+++ script.js
@@ -0,0 +1,24 @@
+// 1
+console.log(9);
+
+// 2
+// Numbers, Strings, Booleans, Null, Undefined
+
+// 3
+// +, -, *, /, **, %
+
+// 4
+console.log(5%2);
+console.log(5%4);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6
+console.log("Hawk" + "eye");
+
+// 7
+console.log("Maianne".length + "Thornton".length);
+
+// 8
+console.log("Avengers: Endgame"[16]);
