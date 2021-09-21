23a3dc14844a351fc62c665985f6e39095ede582
MaianneThornton <maianne.thornton@gmail.com>
2021-09-21 10:53:57 -0400

    Updated with comments from class.

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ console.log(9);
 // 4
 console.log(5%2); // 5 divided by 2 has a remainder of 1
 console.log(5%4); // 5 divided by 4 has a remainder of 1
+// console.log(1%5);
+
 
 // 5
 console.log('I\'M MARY POPPINS, Y\'ALL!');
@@ -25,7 +27,7 @@ console.log("Avengers: Endgame"[16]);
 
 // BONUS BONUS
 // 1
-console.log("/");
+console.log("\\");
 
 // 2
 console.log(5**4-9);
@@ -43,6 +45,13 @@ console.log("DOCTOR STRANGE"[13]);
 console.log("THOR"[3]); 
 console.log("STAR-LORD"[0]);
 
+// To complete bonus question using only 1 console log
+// console.log("CAP"[1] + "\n" + "VISION"[0] + "\n" + "OKOYE"[4] + "\n" + "NICK FURY"[0] + "\n" + "GROOT"[0] + "\n" + "DOCTOR STRANGE"[13] + "\n" + "THOR"[3] + "\n" + "STAR-LORD"[0]);
+
+// To complete bonus question using a JS table...
+// console.table(["CAP"[1], "VISION"[0], "OKOYE"[4], "NICK FURY"[0], "GROOT"[0], "DOCTOR STRANGE"[13], "THOR"[3], "STAR-LORD"[0]]);
+
+// Can also use let or const instead of var
 // 5
 var m = 25;
 var c = 4;
